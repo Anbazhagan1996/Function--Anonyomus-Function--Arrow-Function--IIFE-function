61dcae327a1a3cf727b2742d9f5c3950b2367920
Anbazhagan1996 <Anbazhagan9602@gmail.com>
2023-08-03 03:30:09 +0530

    Function codes

---
+++ script.js
@@ -0,0 +1,75 @@
+
+// 1. Print odd numbers in an array
+// function
+
+// let array = [1,2,3,4,5,6,7,8,9,18];
+// let result = [];
+// function odd(array)
+// {
+//     for(let i=0; i<array.length; i++)
+// {
+//     if(array[i]%2!=0)
+//     {
+//     result.push(array[i]);
+//     }
+// }
+// return result;
+// }
+// console.log(odd(array));
+
+/////////////////////////////////////////////////////////////////////////////
+
+// Anonyomus function
+
+// let array = [1,2,3,4,5,6,7,8,9,18];
+// let result = [];
+// let odd = function(array) 
+// {
+//      for(let i=0; i<array.length; i++)
+//   {
+//       if(array[i]%2!=0)
+//       {
+//       result.push(array[i]);
+//       }
+//   }
+//    return result;
+// };
+// console.log(odd(array));
+
+/////////////////////////////////////////////////////////////////////////////////
+
+// IIFE Function
+
+// (function odd(array)
+//  {
+//     let result = [];
+//     for (let i = 0; i<array.length; i++) 
+//     {
+//       if (array[i]%2!=0) 
+//       {
+//         result.push(array[i]);
+//       }
+//     }
+//     console.log(result);
+// })([1,2,3,4,5,6,7,8,9,18]);
+
+/////////////////////////////////////////////////////////////////////////////////
+
+// Arrow Function
+
+// let odd = (array) => {
+//     let result = [];
+//     for (let i=0; i<array.length; i++)
+//     {
+//         if(array[i]%2!=0)
+//         {
+//         result.push(array[i]);
+//         }
+//     }
+// return result;
+// }
+// console.log(odd([1,2,3,4,5,6,7,8,9,18]))
+
+
+
+  
